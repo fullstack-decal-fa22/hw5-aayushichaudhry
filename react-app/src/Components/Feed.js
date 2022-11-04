2400f12feeba6e347e81d5c5a7e52dc570606912
Aayushi Chaudhry <aayushi.chaudhry@berkeley.edu>
2022-11-04 15:42:24 -0700

    finished

--- react-app/src/Components/Feed.js
+++ react-app/src/Components/Feed.js
@@ -1,39 +1,44 @@
-import React from 'react';
+import React from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Feed = () => {
   // Un-comment the lines below to complete your solution
   // ====================
 
-  // const [_________, _________] = useState();
+  const [data, setData] = useState();
 
-  // const getPostsData = () => {
-  //   axios
-  //     .get(_________) //THIS IS YOUR URL OF YOUR API
-  //     .then(__________) //PROMISE API, THAT MEANS WHEN YOU GET THE DATA WHAT DO I DO WITH IT
-  //     .catch(__________);  //ERROR CATCHING IN CASE WE RECIEVE AN ERROR
-  // };
+  const getPostsData = () => {
+    axios
+      .get("http://localhost:3002/posts") //THIS IS YOUR URL OF YOUR API
+      .then((data) => setData(data.data)) //PROMISE API, THAT MEANS WHEN YOU GET THE DATA WHAT DO I DO WITH IT
+      .catch((error) => console.log(error)); //ERROR CATCHING IN CASE WE RECIEVE AN ERROR
+  };
 
-  // useEffect(() => {
-  //   _________();
-  // }, [])
+  useEffect(() => {
+    getPostsData();
+  }, []);
 
   // ====================
 
   return (
-    <div style={{ maxWidth: '600px', marginLeft: 'auto', marginRight: 'auto'}}>
-      {
-        data && data.map(d =>
-          <Post title={d.title} body={d.body} comments={d.comments} id={d.id} key={d.id} />
-        )
-      }
+    <div style={{ maxWidth: "600px", marginLeft: "auto", marginRight: "auto" }}>
+      {data &&
+        data.map((d) => (
+          <Post
+            title={d.title}
+            body={d.body}
+            comments={d.comments}
+            id={d.id}
+            key={d.id}
+          />
+        ))}
 
-      <NewPost __________ />
+      <NewPost newFunc={getPostsData} />
     </div>
-  )
-
-}
-
+  );
+};
 
 export default Feed;
